6834d017cc4dc7c26010f0d536ab2c740bfb882d
Mritunjay <mritunjaykarnatak44@gmail.com>
2021-06-27 19:23:28 +0530

    third-commit

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent implements OnInit {
-  listOfPosts: Post[]=[
-    new Post("Nature","nature is undefine","","test@test.com",new Date())
-  ];
-  constructor() { }
+  listOfPosts: Post[] = [];
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
+    this.listOfPosts =this.postService.getPosts();
   }
-
 }
---
+++ src/app/post.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+
+@Injectable({ providedIn: 'root' })
+export class PostService {
+  listOfPosts: Post[] = [
+    new Post('Nature', 'nature is undefine', '', 'test@test.com', new Date()),
+  ];
+
+  getPosts(){
+	  return this.listOfPosts;
+  }
+
+  deleteDelete(index:number){
+	  this.listOfPosts.splice(index,1);
+  }
+
+  addPost(post: Post){
+	  this.listOfPosts.push(post);
+  }
+
+  updatePost(index:number,post:Post){
+	  this.listOfPosts[index] =post;
+  }
+  
+}
