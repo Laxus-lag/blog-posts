d422ffcff0bab4089e7f8244fac12bf96ae4254d
Mritunjay <mritunjaykarnatak44@gmail.com>
2021-06-28 00:09:09 +0530

    addingPosts-done

--- src/app/post-edit/post-edit.component.ts
+++ src/app/post-edit/post-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-edit',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-edit.component.css']
 })
 export class PostEditComponent implements OnInit {
-
-  constructor() { }
+  form!:FormGroup;
+  constructor(private postService:PostService,private router:Router) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null ,[Validators.required,Validators.maxLength(75)]),
+      description: new FormControl(null, [Validators.required]),
+      imagePath: new FormControl(null,[Validators.required]),
+    });
+  }
+  onSubmit(){
+    console.log("hello");
+    const title =this.form.value.title;
+    const description = this.form.value.description;
+    const imagePath = this.form.value.imagePath;
+    
+    const post: Post=new Post(title,description,imagePath,"test@Test.com",new Date());
+  
+    this.postService.addPost(post);
+
+    this.router.navigate(["/post-list"]);
   }
 
 }
